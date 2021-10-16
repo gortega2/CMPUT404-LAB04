9266b06419f89844777754ff71bb05835ea63f15
gortega2 <fpsgus97@gmail.com>
2021-10-16 17:58:59 -0600

    Lab 06 Heroku changes

--- mysite/settings.py
+++ mysite/settings.py
@@ -1,4 +1,5 @@
 import os
+import django_on_heroku
 """
 Django settings for mysite project.
 
@@ -120,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+django_on_heroku.settings(locals())
+
+
